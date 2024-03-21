def9046c7a55f202d82a21cbba492e1f980d2e58
Buvan4422 <buvansingh05@gmail.com>
2024-03-21 11:08:42 +0530

    modified:   backend/APIs/author-api.js modified:   backend/APIs/user-api.js modified:   backend/node_modules/.package-lock.json modified:   backend/package-lock.json modified:   backend/package.json modified:   frontend/re.http

--- backend/APIs/author-api.js
+++ backend/APIs/author-api.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const authorapp = express.Router();
 const bcrytpjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 authorapp.use(express.json());
 
 let authorCollection;
-
 authorapp.use((req, res, next) => {
   authorCollection = req.app.get('authorCollection');
   next();
 });
+
+//get method
 authorapp.get('/authors', async (req, res) => {
   let authorsList = await authorCollection.find().toArray();
   res.send({ message: 'your data', payload: authorsList });
 });
 
+//post method
 authorapp.post('/register', async (req, res) => {
   let newAuthor = req.body;
+  let dbAuthor = await authorCollection.findOne({
+    username: newAuthor.username,
+  });
+  if (dbAuthor !== null) {
+    return res.send({ message: 'Author already exists' });
+  }
   let hashedPassword = await bcrytpjs.hash(newAuthor.password, 6);
 
   newAuthor.password = hashedPassword;
@@ -23,20 +32,32 @@ authorapp.post('/register', async (req, res) => {
   res.send({ message: 'New author  created' });
 });
 
-authorapp.put('/update/:username', async (req, res) => {
-  let uname = req.params.username;
-  let updateData = req.body;
-  // Checking for password updates
-  if ('password' in updateData) {
-    updateData.password = await bcrytpjs.hash(updateData.password, 6);
+authorapp.post('/login', async (req, res) => {
+  //get authour credObj
+  let authCred = req.body;
+  let dbAuthor = await authorCollection.findOne({
+    username: authCred.username,
+  });
+  if (dbAuthor === null) {
+    return res.send({ message: 'Invalid Username' });
   } else {
-    delete updateData.password;
+    let result = await bcrytpjs.compare(authCred.password, dbAuthor.password);
+    if (result === false) {
+      return res.send({ message: 'Invalid Password' });
+    } else {
+      //create token
+      let singleToken = jwt.sign({ username: authCred.username }, 'abcedf', {
+        expiresIn: 30,
+      });
+      res.send({
+        message: 'Login Successful',
+        token: singleToken,
+        author: dbAuthor,
+      });
+    }
   }
-  const result = await authorCollection.updateOne(
-    { name: uname },
-    { $set: updateData }
-  );
-  res.send({ message: 'author updated' });
 });
+
+//put method
 
 module.exports = authorapp;
--- backend/APIs/user-api.js
+++ backend/APIs/user-api.js
@@ -1,14 +1,59 @@
 const express = require('express');
 const Userapp = express.Router();
+const bcrytpjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 Userapp.use(express.json());
 
-let userCollections;
+let userCollection;
 Userapp.use((req, res, next) => {
-  userCollections = req.app.get('userCollections');
+  userCollection = req.app.get('userCollection');
   next();
 });
 
-Userapp.get('/test-user', (req, res) => {
-  res.send('From user api');
+Userapp.get('/users', async (req, res) => {
+  let usersList = await userCollection.find().toArray();
+  res.send({ message: 'your data', payload: usersList });
 });
+
+Userapp.post('/register', async (req, res) => {
+  let newUser = req.body;
+  let dbUSer = await userCollection.findOne({
+    username: newUser.username,
+  });
+  if (dbUSer !== null) {
+    return res.send({ message: 'User already exists' });
+  }
+  let hashedPassword = await bcrytpjs.hash(newUser.password, 6);
+
+  newUser.password = hashedPassword;
+  await userCollection.insertOne(newUser);
+  res.send({ message: 'New User created' });
+});
+
+Userapp.post('/login', async (req, res) => {
+  //get authour credObj
+  let userCred = req.body;
+  let dbUser = await userCollection.findOne({
+    username: userCred.username,
+  });
+  if (dbUser === null) {
+    return res.send({ message: 'Invalid Username' });
+  } else {
+    let result = await bcrytpjs.compare(userCred.password, dbUser.password);
+    if (result === false) {
+      return res.send({ message: 'Invalid Password' });
+    } else {
+      //create token
+      let singleToken = jwt.sign({ username: userCred.username }, 'abcedf', {
+        expiresIn: 30,
+      });
+      res.send({
+        message: 'Login Successful',
+        token: singleToken,
+        author: dbUser,
+      });
+    }
+  }
+});
+
 module.exports = Userapp;
